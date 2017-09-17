be1a6a7c3404d644bc0a444d564a69d3ef602680
rabex028 <rabex028@morris.umn.edu>
2017-09-17 12:44:58 -0500

    Add Todo Filtering by Owner w/ Tests
    Owner filtering is now implemented for todos and
    the tests all pass.

---
+++ src/test/java/umm3601/todo/FilterTodoByOwnerFromDB.java
@@ -0,0 +1,42 @@
+package umm3601.todo;
+
+
+import org.junit.Test;
+import umm3601.entries.Todo;
+import umm3601.entries.Database;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+
+public class FilterTodoByOwnerFromDB {
+
+  @Test
+  public void  filterTodosByOwner() throws IOException {
+    Database db = new Database("src/main/data/todos.json");
+    Todo[] allTodos = db.listTodos(new HashMap<>());
+
+    Todo[] ownerBarryTodos = db.filterTodosByOwner(allTodos, "Barry");
+    assertEquals("Incorrect number of users for owner Barry", 51, ownerBarryTodos.length);
+
+    Todo[] ownerDawnTodos = db.filterTodosByOwner(allTodos, "Dawn");
+    assertEquals("Incorrect number of users for ownwer Dawn", 50, ownerDawnTodos.length);
+  }
+
+  @Test
+  public void listTodosWithOwnerFilter() throws IOException {
+    Database db = new Database("src/main/data/todos.json");
+    Map<String, String[]> queryParams = new HashMap<>();
+
+    queryParams.put("owner", new String[] {"Barry"});
+    Todo[] ownerBarryTodos = db.listTodos(queryParams);
+    assertEquals("Incorrect number of todos for owner Barry", 51, ownerBarryTodos.length);
+
+    queryParams.put("owner", new String[] {"Dawn"});
+    Todo[] ownerDawnTodos = db.listTodos(queryParams);
+    assertEquals("Incorrect number of todos for owner Dawn", 50, ownerDawnTodos.length);
+  }
+
+}
