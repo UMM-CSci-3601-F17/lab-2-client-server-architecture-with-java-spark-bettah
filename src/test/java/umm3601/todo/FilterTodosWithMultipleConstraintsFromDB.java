0f0742b19b828be7649b1a07b49a3a773e241a45
rabex028 <rabex028@morris.umn.edu>
2017-09-18 20:10:18 -0500

    Add Second test for multiple queries
    A second test was added for handling requests with multiple query
    parameters. This test actually places the properties to filter by
    into the queryParameters format and only filters by two properties.

--- src/test/java/umm3601/todo/FilterTodosWithMultipleConstraintsFromDB.java
+++ src/test/java/umm3601/todo/FilterTodosWithMultipleConstraintsFromDB.java
@@ -23,7 +23,7 @@ public class FilterTodosWithMultipleConstraintsFromDB {
    filteredTodos = db.filterTodosByCategory(filteredTodos, "groceries");
    filteredTodos = db.filterTodosByContent(filteredTodos, "dolor");
    filteredTodos = db.filterTodosByOwner(filteredTodos, "Barry");
-   // limits filteredTodos from 3 to 2
+   // limits filteredTodos from what would be length 3 to 2
    filteredTodos = db.limitTodos(filteredTodos, 2);
    filteredTodos = db.orderTodos(filteredTodos, "body");
 
@@ -31,26 +31,46 @@ public class FilterTodosWithMultipleConstraintsFromDB {
     // Note that filteredTodos should now contain only 2 Todos, so we resort to checking both for their properties.
 
     //Checks Limit filter
-    assertEquals("Incorrect Length", 2, filteredTodos.length);
+    assertEquals("Incorrect Length of filteredTodos", 2, filteredTodos.length);
 
     //Checks Status filter
-    assertEquals("Incorrect Status",true,filteredTodos[0].status);
-    assertEquals("Incorrect Status",true,filteredTodos[1].status);
+    assertEquals("Incorrect Status of Todo found",true,filteredTodos[0].status);
+    assertEquals("Incorrect Status of Todo found",true,filteredTodos[1].status);
 
     //Checks category filter
-    assertEquals("Incorrect Category","groceries",filteredTodos[0].category);
-    assertEquals("Incorrect Category","groceries",filteredTodos[1].category);
+    assertEquals("Incorrect Category of Todo found","groceries",filteredTodos[0].category);
+    assertEquals("Incorrect Category of Todo found","groceries",filteredTodos[1].category);
 
     //Checks body filter
-    assertEquals("Incorrect Content",true,filteredTodos[0].body.contains("dolor"));
-    assertEquals("Incorrect Content",true,filteredTodos[1].body.contains("dolor"));
+    assertEquals("Incorrect Content of Todo found",true,filteredTodos[0].body.contains("dolor"));
+    assertEquals("Incorrect Content of Todo found",true,filteredTodos[1].body.contains("dolor"));
 
     //Checks owner filter
-    assertEquals("Incorrect Owner","Barry",filteredTodos[0].owner);
-    assertEquals("Incorrect Owner","Barry",filteredTodos[1].owner);
+    assertEquals("Incorrect Owner of Todo found","Barry",filteredTodos[0].owner);
+    assertEquals("Incorrect Owner of Todo found","Barry",filteredTodos[1].owner);
 
     //Checks expected order
-    assertEquals("Incorrect order", true, (filteredTodos[0].body.compareTo(filteredTodos[1].body) < 0));
+    assertEquals("Incorrect order of Todos found", true, (filteredTodos[0].body.compareTo(filteredTodos[1].body) < 0));
 
+  }
+
+  @Test
+  public void testMultipleQueryParams() throws IOException {
+    Database db = new Database("src/main/data/todos.json");
+    Map<String, String[]> queryParams = new HashMap<>();
+    boolean todoFilterError = false;
+
+    queryParams.put("status", new String[] {"true"});
+    queryParams.put("owner", new String[] {"Dawn"});
+
+    Todo[] DawnCompletedTasks = db.listTodos(queryParams);
+
+
+    for (int i = 0; i < DawnCompletedTasks.length; i++) {
+       if (DawnCompletedTasks[i].status != true || DawnCompletedTasks[i].owner != "Dawn") {
+         todoFilterError = true;
+       }
+       assertEquals("Incorrect property of Todo found", true, todoFilterError);
+    }
   }
 }
